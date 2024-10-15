587db2264faf3ecee62b02f3aafc8548566ff6ef
KeceK <78042511+Kecek05@users.noreply.github.com>
2024-10-15 15:49:01 -0300

    v1
    player anim

--- Assets/comandos/Player_Movi.cs
+++ Assets/comandos/Player_Movi.cs
@@ -8,13 +8,43 @@ public class Player_Movi : MonoBehaviour
     public float speed;
     private Rigidbody2D rig;
     private Vector2 direction;
+
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+
     private void Start()
     {
         rig = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
     private void Update()
     {
         direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+
+        if (direction.magnitude > 0)
+        {
+            //walking
+            animator.SetBool("isWalking", true);
+
+            
+            if (direction.x < 0)
+            {
+                spriteRenderer.flipX = false; // flip left
+            }
+            else if (direction.x > 0)
+            {
+                spriteRenderer.flipX = true; // flip right
+            }
+
+        }
+        else
+        {
+            // idle
+            animator.SetBool("isWalking", false);
+        }
+
     }
     private void FixedUpdate()
     {
