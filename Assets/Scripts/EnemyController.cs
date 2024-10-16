8ec2c1d72604ed2f4ee8633f8f12beadb56159f3
Kecek05 <78042511+Kecek05@users.noreply.github.com>
2024-10-16 18:29:48 -0300

    v7
    enemy anim and flip

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -8,17 +8,33 @@ public class EnemyController : MonoBehaviour
 
     [SerializeField] private NavMeshAgent agent;
     [SerializeField] private GameObject player;
-    // Start is called before the first frame update
+    private SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
     void Start()
     {
         //cuz its 2D, not rotate
         agent.updateRotation = false;
         agent.updateUpAxis = false;
+
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         agent.SetDestination(player.transform.position);
+
+        if (player.transform.position.x < transform.position.x)
+        {
+            spriteRenderer.flipX = false;
+        }
+        else if (player.transform.position.x > transform.position.x)
+        {
+            spriteRenderer.flipX = true;
+        }
     }
 }
