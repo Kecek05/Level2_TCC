5d85538caab32ad67a7337c144802aa399534cc7
João Felipe <jfk.abrao@hotmail.com>
2024-10-29 16:43:49 -0300

    v9
    Anim Attack

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+
+    [SerializeField] private GameObject[] enemySpawners;
+    [SerializeField] private GameObject enemyPrefab;
+    void Start()
+    {
+        StartCoroutine(EnemySpawnerCoroutine());
+    }
+
+    private IEnumerator EnemySpawnerCoroutine()
+    {
+        while (true)
+        {
+            int randomIndex = Random.Range(0, enemySpawners.Length);
+            Instantiate(enemyPrefab, enemySpawners[randomIndex].transform);
+
+            yield return new WaitForSeconds(5f);
+        }
+    }
+
+}
--- Assets/Scripts/Player_Movi.cs
+++ Assets/Scripts/Player_Movi.cs
@@ -12,6 +12,11 @@ public class Player_Movi : MonoBehaviour
     private Animator animator;
     private SpriteRenderer spriteRenderer;
 
+    private float attackCoolDown = 2f;
+    private bool isAttacking;
+    [SerializeField] private GameObject attackPoint;
+    [SerializeField] private LayerMask enemyLayer;
+
     private void Awake()
     {
         rig = GetComponent<Rigidbody2D>();
@@ -20,6 +25,12 @@ public class Player_Movi : MonoBehaviour
     }
     private void Update()
     {
+        if(Input.GetMouseButtonDown(0) && !isAttacking)
+        {
+            isAttacking = true;
+        }
+
+
         direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 
 
@@ -51,4 +62,30 @@ public class Player_Movi : MonoBehaviour
         rig.MovePosition(rig.position + direction * speed * Time.fixedDeltaTime);
     }
 
+
+    private IEnumerator AttackCoroutine()
+    {
+        animator.SetBool("isAttacking", isAttacking);
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.transform.position, 10f, enemyLayer);
+
+        // Itera sobre todos os inimigos atingidos
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            Destroy(enemy.gameObject); // Destrói o inimigo atingido
+            Debug.Log("Inimigo destruído: " + enemy.name);
+        }
+
+        yield return new WaitForSeconds(1f);
+        isAttacking = false;
+        animator.SetBool("isAttacking", isAttacking);
+    }
+
+    //Debug
+    private void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null) return;
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPoint.transform.position, 10f);
+    }
 }
