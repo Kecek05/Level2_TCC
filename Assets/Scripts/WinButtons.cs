30e432ee34adb0f32449c8fd692ad8fc9c7a2d2f
KeceK <jfk.abrao@hotmail.com>
2024-11-19 18:29:11 -0300

    v16

---
+++ Assets/Scripts/WinButtons.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinButtons : MonoBehaviour
+{
+
+    public void PlayAgainBtn()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void QuitBtn()
+    {
+        Application.Quit();
+    }
+}
